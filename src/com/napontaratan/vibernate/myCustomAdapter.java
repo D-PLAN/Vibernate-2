3f031ca44a41fa0ad01b4a4f6e38a275aa320728
Aor_N <scc385@gmail.com>
2015-03-07 17:45:52 -0800

    I think I successfully setup the listFragment

--- src/com/napontaratan/vibernate/myCustomAdapter.java
+++ src/com/napontaratan/vibernate/myCustomAdapter.java
@@ -1,9 +1,28 @@
 package com.napontaratan.vibernate;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by Aor-Nawattranakul on 15-03-07.
  */
-public class myCustomAdapter {
+public class myCustomAdapter extends ArrayAdapter{
+    private Context context;
+    private boolean uselist = true;
+
+
+    public myCustomAdapter(Context context, int hi) {
+        super(context, hi);
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int Position, View contentView, ViewGroup parent) {
+
+        return null;
+    }
 
 
 }
