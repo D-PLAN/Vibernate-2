340802455f330406a8283c25346a2e7d0be1e14f
Aor_N <scc385@gmail.com>
2015-04-06 14:03:44 -0700

    Implemented onBindViewHolder

--- Vibernate-2/src/main/java/com/napontaratan/vibernate/vAdapter.java
+++ Vibernate-2/src/main/java/com/napontaratan/vibernate/vAdapter.java
@@ -8,20 +8,24 @@ import android.view.ViewGroup;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Aor-Nawattranakul on 15-04-06.
  */
 public class vAdapter extends RecyclerView.Adapter<vAdapter.vViewHolder> {
     private LayoutInflater inflater;
+    List<vInfo> data = Collections.emptyList(); // this is so that we won't be getting nullpointer exception
 
 
-    public vAdapter(Context context) {
+    public vAdapter(Context context, List<vInfo> data) {
         inflater = LayoutInflater.from(context);
+        this.data = data;
     }
 
 
     @Override
-
     public vViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = inflater.inflate(R.layout.v_row, parent, false);
         vViewHolder holder = new vViewHolder(view);
@@ -30,7 +34,10 @@ public class vAdapter extends RecyclerView.Adapter<vAdapter.vViewHolder> {
 
     @Override
     public void onBindViewHolder(vViewHolder holder, int position) {
-
+        vInfo current = data.get(position);
+        holder.description.setText(current.descrition);
+        holder.startTime.setFormat12Hour(current.startTime);
+        holder.endTime.setFormat12Hour(current.endTime);
     }
 
     @Override
---
+++ Vibernate-2/src/main/java/com/napontaratan/vibernate/vInfo.java
@@ -0,0 +1,11 @@
+package com.napontaratan.vibernate;
+
+/**
+ * Created by Aor-Nawattranakul on 15-04-06.
+ */
+public class vInfo {
+    String descrition;
+    String startTime;
+    String endTime;
+
+}
