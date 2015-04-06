d693bfb09c6e2d931588e260917eaeb1705b3b9a
Napon Taratan <napon@napontaratan.com>
2015-04-06 14:17:04 -0700

    added dynamic days selection and animation to buttons in create timer activity

--- Vibernate-2/src/main/java/com/napontaratan/vibernate/CreateTimerActivity.java
+++ Vibernate-2/src/main/java/com/napontaratan/vibernate/CreateTimerActivity.java
@@ -5,23 +5,28 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 import com.napontaratan.vibernate.view.CreateTimerTimePicker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateTimerActivity extends FragmentActivity {
 
     static final int TIME_DIALOG = 0;
     DialogFragment timePicker;
+    List<ToggleButton> days;
 
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.create_timer);
-
         timePicker = new CreateTimerTimePicker();
 
+        /* toolbar */
         Toolbar toolbar = (Toolbar) findViewById(R.id.create_timer_toolbar);
         toolbar.setTitle("New Timer");
         toolbar.setNavigationIcon(R.drawable.abc_ic_clear_mtrl_alpha);
@@ -32,9 +37,11 @@ public class CreateTimerActivity extends FragmentActivity {
             }
         });
 
+        /* name field */
         EditText nameField = (EditText) findViewById(R.id.create_timer_name_field);
         nameField.clearFocus();
 
+        /* vibrate or silent mode */
         final ToggleButton typeVibrate = (ToggleButton) findViewById(R.id.create_timer_type_vibrate);
         final ToggleButton typeSilent  = (ToggleButton) findViewById(R.id.create_timer_type_silent);
         typeVibrate.setChecked(true);
@@ -56,6 +63,8 @@ public class CreateTimerActivity extends FragmentActivity {
             }
         });
 
+
+        /* start time & end time */
         final TextView startTime = (TextView) findViewById(R.id.create_timer_start_time_clock);
         final TextView endTime = (TextView) findViewById(R.id.create_timer_end_time_clock);
 
@@ -76,5 +85,79 @@ public class CreateTimerActivity extends FragmentActivity {
             }
         });
 
+        /* choosing days */
+        final ToggleButton weekdays_btn = (ToggleButton) findViewById(R.id.create_timer_weekdays_btn);
+        final ToggleButton weekends_btn = (ToggleButton) findViewById(R.id.create_timer_weekends_btn);
+
+        days = new ArrayList<ToggleButton>();
+        days.add((ToggleButton) findViewById(R.id.create_timer_sun));
+        days.add((ToggleButton) findViewById(R.id.create_timer_mon));
+        days.add((ToggleButton) findViewById(R.id.create_timer_tue));
+        days.add((ToggleButton) findViewById(R.id.create_timer_wed));
+        days.add((ToggleButton) findViewById(R.id.create_timer_thu));
+        days.add((ToggleButton) findViewById(R.id.create_timer_fri));
+        days.add((ToggleButton) findViewById(R.id.create_timer_sat));
+
+        days.get(0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (days.get(0).isChecked() && days.get(6).isChecked()) weekends_btn.setChecked(true);
+                else weekends_btn.setChecked(false);
+            }
+        });
+        days.get(6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (days.get(0).isChecked() && days.get(6).isChecked()) weekends_btn.setChecked(true);
+                else weekends_btn.setChecked(false);
+            }
+        });
+
+        for(int i = 1; i < 6; i++){
+            final int finalI = i;
+            days.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(days.get(finalI).isChecked()) {
+                        for(int j = 1; j < 6; j++){
+                            if(!days.get(j).isChecked()) return;
+                        }
+                        weekdays_btn.setChecked(true);
+                    } else {
+                        weekdays_btn.setChecked(false);
+                    }
+                }
+            });
+        }
+
+        weekdays_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(weekdays_btn.isChecked()) {
+                    for(int i = 0; i < 7; i++){
+                        if(i != 0 && i != 6) days.get(i).setChecked(true);
+                    }
+                } else {
+                    for(int i = 0; i < 7; i++){
+                        if(i != 0 && i != 6) days.get(i).setChecked(false);
+                    }
+                }
+            }
+        });
+
+        weekends_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(weekends_btn.isChecked()) {
+                    for(int i = 0; i < 7; i++){
+                        if(i == 0 || i == 6) days.get(i).setChecked(true);
+                    }
+                } else {
+                    for(int i = 0; i < 7; i++){
+                        if(i == 0 || i == 6) days.get(i).setChecked(false);
+                    }
+                }
+            }
+        });
     }
 }
