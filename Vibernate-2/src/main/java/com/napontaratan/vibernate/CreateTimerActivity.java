3fc7d7a461a4353d5674d4475a284310220ec909
Napon Taratan <napon@napontaratan.com>
2015-04-06 13:50:23 -0700

    added timepicker popup and horizontal dividers

--- Vibernate-2/src/main/java/com/napontaratan/vibernate/CreateTimerActivity.java
+++ Vibernate-2/src/main/java/com/napontaratan/vibernate/CreateTimerActivity.java
@@ -1,25 +1,26 @@
 package com.napontaratan.vibernate;
 
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextClock;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import android.widget.*;
+import com.napontaratan.vibernate.view.CreateTimerTimePicker;
 
-
-public class CreateTimerActivity extends Activity {
+public class CreateTimerActivity extends FragmentActivity {
 
     static final int TIME_DIALOG = 0;
+    DialogFragment timePicker;
 
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.create_timer);
+
+        timePicker = new CreateTimerTimePicker();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.create_timer_toolbar);
         toolbar.setTitle("New Timer");
@@ -31,19 +32,8 @@ public class CreateTimerActivity extends Activity {
             }
         });
 
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                switch(menuItem.getItemId()) {
-                    case(R.id.create_timer_save):
-                        // TODO: save
-                        return true;
-                }
-                return false;
-            }
-        });
-
-        toolbar.inflateMenu(R.menu.create_timer_toolbar);
+        EditText nameField = (EditText) findViewById(R.id.create_timer_name_field);
+        nameField.clearFocus();
 
         final ToggleButton typeVibrate = (ToggleButton) findViewById(R.id.create_timer_type_vibrate);
         final ToggleButton typeSilent  = (ToggleButton) findViewById(R.id.create_timer_type_silent);
@@ -66,8 +56,8 @@ public class CreateTimerActivity extends Activity {
             }
         });
 
-        TextView startTime = (TextView) findViewById(R.id.create_timer_start_time_clock);
-        TextView endTime = (TextView) findViewById(R.id.create_timer_end_time_clock);
+        final TextView startTime = (TextView) findViewById(R.id.create_timer_start_time_clock);
+        final TextView endTime = (TextView) findViewById(R.id.create_timer_end_time_clock);
 
         startTime.setText("15:00"); // TODO
         endTime.setText("21:00");   // TODO
@@ -75,14 +65,14 @@ public class CreateTimerActivity extends Activity {
         startTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(), "TODO!", Toast.LENGTH_SHORT).show();
+                timePicker.show(getSupportFragmentManager(), "startTimePicker");
             }
         });
 
         endTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(), "TODO!", Toast.LENGTH_SHORT).show();
+                timePicker.show(getSupportFragmentManager(), "endTimePicker");
             }
         });
 
