3f006b98a0b9150498eca7f1d3ecebcc337ab80d
Napon Taratan <napon@napontaratan.com>
2015-04-26 17:29:58 -0700

    fixed silly bug

---
+++ Vibernate-2/src/main/java/com/napontaratan/vibernate/view/VibernateTutorial.java
@@ -0,0 +1,10 @@
+package com.napontaratan.vibernate.view;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by napontaratan on 15-04-26.
+ */
+public class VibernateTutorial {
+
+}
