ed62db8be290a8083843583a094f2a00a56102db
amelia-ylh <amelia_ylh@hotmail.com>
2015-05-24 16:48:25 -0700

    remove unused import

--- Vibernate-2/src/main/java/com/napontaratan/vibernate/view/TimerWeekView.java
+++ Vibernate-2/src/main/java/com/napontaratan/vibernate/view/TimerWeekView.java
@@ -9,7 +9,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.*;
